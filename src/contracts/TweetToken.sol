950b0c488e4af6b58a5830de1d3c6a6eeaf8c018
James Kane <james.kane59@outlook.com>
2023-03-28 11:08:56 +0100

    create TWEET token contract

---
+++ src/contracts/TweetToken.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+contract Token {
+
+    // Variables
+    string public name = "Tweet Token";
+    string public symbol = 'TWEET';
+    uint256 public decimals = 18;
+    uint256 public totalSupply;
+
+    // Track Balances
+    mapping(address => uint256) public balanceOf;
+    // deployer => exchange => allowance amount
+    mapping(address => mapping(address => uint256)) public allowance;
+    
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+
+    constructor() {
+        totalSupply = 1000000 * (10 ** 18);
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value);
+        _transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function _transfer(address _from, address _to, uint256 _value) internal {
+        require(_to != address(0));
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(_from, _to, _value);
+    }
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        require(_spender != address(0));
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }   
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value <= balanceOf[_from]);
+        require(_value <= allowance[_from][msg.sender]);
+        allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+        _transfer(_from, _to, _value);
+        return true;
+    }
+}
