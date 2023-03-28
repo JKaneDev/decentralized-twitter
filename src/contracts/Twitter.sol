65d1849bf15a6831d1032f90a83a49fa26db5caa
James Kane <james.kane59@outlook.com>
2023-03-28 14:29:36 +0100

    add user account, update account info, remove user

--- src/contracts/Twitter.sol
+++ src/contracts/Twitter.sol
@@ -8,6 +8,48 @@ contract Twitter {
         owner = _owner;
     }
 
+    struct User {
+        string name;
+        string bio;
+        string profilePictureURL;
+        bool exists;
+    }
+
+    mapping(address => User) public users;
+
+    function createAccount(string memory _name, string memory _bio, string memory _profilePictureURL) public {
+        // Check if user doesn't alreadty exist
+        require(!users[msg.sender].exists, "User already exists");
+
+        // Create a new user and store it in the mapping
+        users[msg.sender] = User({
+            name: _name.
+            bio: _bio,
+            profilePictureURL: _profilePictureURL,
+            exists: true,
+        })
+    }
+
+    function updateName(string memory _name) public {
+        require(users[msg.sender].exists, "User does not exist");
+        users[msg.sender].name = _name;
+    }
+
+    function updateBio(string memory _bio) public {
+        require(users[msg.sender].exists, "User does not exist");
+        users[msg.sender].bio = _bio;
+    }
+
+    function updateProfilePicture(string memory _url) public {
+        require(users[msg.sender].exists, "User does not exist");
+        users[msg.sender].profilePictureURL = _url;
+    }
+
+    function removeUser() public {
+        require(users[msg.sender].exists, "User does not exist");
+        delete users[msg.sender];
+    }
+
     function receiveFunds() external payable {
         // Funds will be received and stored in the contract
     }
