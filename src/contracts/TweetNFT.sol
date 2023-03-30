59747469961f71d3f2a936ec084c68d7e67b117b
James Kane <james.kane59@outlook.com>
2023-03-30 12:46:29 +0100

    give royalties to originalOwners in future sales

--- src/contracts/TweetNFT.sol
+++ src/contracts/TweetNFT.sol
@@ -8,30 +8,31 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 contract TweetNFT is ERC721, ERC721URIStorage {
 
     using Counters for Counters.Counter;
-    Counters.Counter private _tokenIds;
+    Counters.Counter private _nftIDs;
     // Keep track of which tweets have already been minted
     mapping(string => bool) private _tweetMinted;
     // Store the owner of each tweet URI
-    mapping(string => address) private _tweetOwners;
+    mapping(string => address) private _originalOwners;
 
     constructor() ERC721("TweetNFT", "TWNFT") {}
 
-    function mintTweetNFT(address payable recipient, string memory tokenURI, uint256 price) public returns (uint256) {
+    function createAuction(uint256 _nftId, uint256 _startingPrice, uint256 _auctionDuration) external {
+        require(isApprovedOrOwner(msg.sender, _nftId), "Caller is not owner nor approved");
+        Auction newAuction = new Auction(payable(_originalOwners[_nftId]), payable(msg.sender), _nftId, _startingPrice, _auctionDuration, address(this));
+        _approve(address(newAuction), _nftId);
+    }
+
+    function mintTweetNFT(address payable recipient, string memory tokenURI) public returns (uint256) {
         // Only 1 mint per tweet
         require(!_tweetMinted[tokenURI], "Tweet has already been minted");
     
-        _tokenIds.increment();
-        uint256 newItemId = _tokenIds.current();
+        _nftIDs.increment();
+        uint256 newItemId = _nftIDs.current();
         _mint(recipient, newItemId);
         _setTokenURI(newItemId, tokenURI);
 
         _tweetMinted[tokenURI] = true;
-        _tokenOwners[tokenURI] = recipient;
-
-        // Create a new auction for the NFT
-        Auction newAuction = new Auction(recipient, newItemId, price, auctionDuration, address(this));
-        // Approve the auction contract to manage the NFT
-        _approve(address(newAuction), newItemId);
+        _originalOwners[tokenURI] = recipient;
 
         return newItemId;
     }
