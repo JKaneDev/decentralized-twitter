e652632c0eeac573bcfb650b0dd6cd447caf1516
James Kane <james.kane59@outlook.com>
2023-04-01 13:59:29 +0100

    test Twitter.sol deployment

--- test/Twitter.test.js
+++ test/Twitter.test.js
@@ -1,9 +1,10 @@
+const helpers = require('./helpers');
 require('chai').use(require('chai-as-promised')).should();
 const Twitter = artifacts.require('./Twitter');
 const TweetToken = artifacts.require('./TweetToken');
 const TweetNFT = artifacts.require('./TweetNFT');
 
-contract('Twitter', ([deployer]) => {
+contract('Twitter', ([owner, user1, user2]) => {
 	let twitter;
 	let tweetToken;
 	let nft;
@@ -11,14 +12,32 @@ contract('Twitter', ([deployer]) => {
 	beforeEach(async () => {
 		tweetToken = await TweetToken.new();
 		nft = await TweetNFT.new();
-		twitter = await Twitter.new(deployer, tweetToken.address, nft.address);
+		twitter = await Twitter.new(owner, tweetToken.address, nft.address);
 		await nft.setTwitterContractAddress(twitter.address);
+
+		tweetToken.transfer(user1, helpers.tokens(100), { from: owner });
+		tweetToken.transfer(user2, helpers.tokens(100), { from: owner });
 	});
 
 	describe('deployment', async () => {
 		it('tracks the twitter contract owner', async () => {
 			const result = await twitter.owner();
-			result.should.equal(deployer);
+			result.should.equal(owner);
+		});
+
+		it('tracks the tweet token contract address', async () => {
+			const result = await twitter.tweetToken();
+			result.should.equal(tweetToken.address);
+		});
+
+		it('tracks the nft contract address', async () => {
+			const result = await twitter.tweetNFT();
+			result.should.equal(nft.address);
+		});
+
+		it('nft baseURI is IPFS URL', async () => {
+			const result = await twitter.baseURI();
+			result.should.equal('https://ipfs.io/ipfs/');
 		});
 	});
 });
---
+++ test/helpers.js
@@ -0,0 +1,24 @@
+const utils = require('web3').utils;
+
+const ether = (n) => {
+	return new utils.BN(utils.toWei(n.toString(), 'ether'));
+};
+
+const tokens = (n) => ether(n);
+
+const ETH_ADDRESS = '0x0000000000000000000000000000000000000000';
+
+const EVM_REVERT = 'VM Exception while processing transaction: revert';
+
+const wait = (seconds) => {
+	const milliseconds = seconds * 1000;
+	return new Promise((resolve) => setTimeout(resolve, milliseconds));
+};
+
+module.exports = {
+	ether,
+	tokens,
+	ETH_ADDRESS,
+	EVM_REVERT,
+	wait,
+};
