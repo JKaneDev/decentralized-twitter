0c19eb09b936ca9cc1d704de0e322112ddaa19aa
James Kane <james.kane59@outlook.com>
2023-04-08 20:23:09 +0100

    switch to client side blockchain data rendering to avoid dispatch issues

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,37 +1,28 @@
 import styles from '@components/styles/Home.module.css';
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
-import { loadWeb3, loadAccount, loadTweetToken, loadTweetNFT, loadTwitter, loadAuction } from '../store/interactions';
+import { useDispatch } from 'react-redux';
+import { loadWeb3, loadAccount, loadTweetToken, loadTweetNFT, loadTwitter } from '../store/interactions';
 
-async function loadBlockchainData(dispatch) {
-	const web3 = await loadWeb3(dispatch);
-	const networkId = await web3.eth.net.getId();
-	await loadAccount(web3, dispatch);
-	const tweetToken = await loadTweetToken(web3, networkId, dispatch);
-	const tweetNFT = await loadTweetNFT(web3, networkId, dispatch);
-	const twitter = await loadTwitter(web3, networkId, dispatch);
+const Home = (props) => {
+	useEffect(() => {
+		loadBlockchainData(props.dispatch);
+	}, []);
 
-	return { tweetToken, tweetNFT, twitter };
-}
+	async function loadBlockchainData(dispatch) {
+		const web3 = await loadWeb3(dispatch);
+		const networkId = await web3.eth.net.getId();
+		await loadAccount(web3, dispatch);
+		const tweetToken = await loadTweetToken(web3, networkId, dispatch);
+		const tweetNFT = await loadTweetNFT(web3, networkId, dispatch);
+		const twitter = await loadTwitter(web3, networkId, dispatch);
+	}
 
-const Home = ({ tweetToken, tweetNFT, twitter }) => {
 	return <></>;
 };
 
 function mapStateToProps(state) {
 	return {};
-}
-
-export async function getServerSideProps() {
-	const dispatch = useDispatch();
-	const blockchainData = await loadBlockchainData(dispatch);
-
-	return {
-		props: {
-			tweetToken: blockchainData.tweetToken,
-			tweetNFT: blockchainData.tweetNFT,
-			twitter: blockchainData.twitter,
-		},
-	};
 }
 
 export default connect(mapStateToProps)(Home);
